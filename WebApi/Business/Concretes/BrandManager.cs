40bc6de99ce2c3325897a512046645420fc790f2
yunskaya <yunusky38@outlook.com>
2024-02-25 23:46:26 +0300

    Ders 3 commit 1

---
+++ WebApi/Business/Concretes/BrandManager.cs
@@ -0,0 +1,39 @@
+﻿using Business.Abstracts;
+using Business.Dtos.Requests;
+using Business.Dtos.Responses;
+using DataAccess.Abstracts;
+using Entities.Concretes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concretes
+{
+    public class BrandManager : IBrandService
+    {
+        private readonly IBrandDal _brandDal;
+
+        public BrandManager(IBrandDal brandDal)
+        {
+            _brandDal = brandDal;
+        }
+
+        public CreatedBrandResponse Add(CreateBrandRequest createBrandRequest)
+        {
+            //business rules
+            Brand brand = new();
+            brand.Name = createBrandRequest.Name;
+
+            _brandDal.Add(brand);
+
+            CreatedBrandResponse 
+        }
+
+        public List<GetAllBrandResponse> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WebApi/DataAccess/Concretes/BrandDal.cs
@@ -0,0 +1,33 @@
+﻿using DataAccess.Abstracts;
+using Entities.Concretes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concretes
+{
+    internal class BrandDal : IBrandDal
+    {
+        List<Brand> _brands;
+        public BrandDal()
+        {
+            _brands = new List<Brand>();
+            _brands.Add(new Brand() { Id = 1, Name = "BMW", CreatedDate = DateTime.Now });
+            _brands.Add(new Brand() { Id = 2, Name = "Mercedes", CreatedDate = DateTime.Now });
+            _brands.Add(new Brand() { Id = 3, Name = "Doblo", CreatedDate = DateTime.Now });
+
+        }
+
+        public void Add(Brand brand)
+        {
+            _brands.Add(brand);
+        }
+
+        public List<Brand> GetAll()
+        {
+            return _brands;
+        }
+    }
+}
